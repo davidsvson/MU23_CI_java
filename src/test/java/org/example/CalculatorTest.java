a4c425c4c73a6bc8487e3312891360ae7b139ec1
David Svensson <davidsvson@gmail.com>
2024-10-07 09:13:47 +0200

    first commit

---
+++ src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @org.junit.jupiter.api.Test
+    void add() {
+        // given
+        Calculator calc = new Calculator();
+
+        //when
+        int result = calc.add(3, 5);
+
+        //then
+        assertEquals(8, result);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void subtract() {
+        Calculator calc = new Calculator();
+
+        //when
+        int result = calc.subtract(3, 5);
+
+        //then
+        assertEquals(8, result);
+
+
+    }
+}
